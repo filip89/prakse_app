863a7f2823c294c7bf0412ea05d1ef77caca1da9
filip89 <filip187m@gmail.com>
2016-08-15 17:42:43 +0200

    Update Internship.php

--- app/Internship.php
+++ app/Internship.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Internship extends Model
 {
-    public function company() {
+    public function companies() {
 
     	return $this->belongsToMany('Apps/Company');
     	
