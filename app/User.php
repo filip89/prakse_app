a07da7bf346ae6ab30f352678d372d10837966ca
filip89 <filip187m@gmail.com>
2016-08-15 20:13:42 +0200

    Update User.php

--- app/User.php
+++ app/User.php
@@ -38,14 +38,51 @@ class User extends Authenticatable
 	
 	public function applic() {
 		
-		return $this->hasOne('App\Applic');
+		return $this->hasOne('App\Applic', 'student_id');
 	
 	}
 	
+	//for student	
 	public function internship() {
 		
-		return $this->hasOne('App\Internship');
+		return $this->hasOne('App\Internship', 'student_id');
+		
+	}
+	
+	//for mentors
+	public function internships() {
+		
+		if($this->role == "intern_mentor"){
+			
+			return $this->hasMany('App\Internship', 'intern_mentor_id');
+			
+		}
+		elseif($this->role == "college_mentor"){
+			
+			return $this->hasMany('App\Internship', 'college_mentor_id');
+			
+		}
 	
 	}
+	
+	/*
+	public function i_mentor_internships() {
+		
+		return $this->hasMany('App\Internship', 'intern_mentor_id');
+	
+	}
+	
+	public function c_mentor_internships() {
+		
+		return $this->hasMany('App\Internship', 'college_mentor_id');
+	
+	}
+	
+	public function a_mentor_internships() {
+		
+		return $this->hasMany('App\Internship', 'applied_mentor_id');
+	
+	}
+	*/
 	
 }
