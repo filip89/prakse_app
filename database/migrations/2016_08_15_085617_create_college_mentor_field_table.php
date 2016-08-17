6c688d4cf5ddbf9bf5ae2b8586461a9a43038ae2
filip89 <filip187m@gmail.com>
2016-08-17 11:53:32 +0200

    Update and rename 2016_08_15_085617_create_field_mentor_table.php to 2016_08_15_085617_create_college_mentor_field_table.php

--- database/migrations/2016_08_15_085617_create_college_mentor_field_table.php
+++ database/migrations/2016_08_15_085617_create_college_mentor_field_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateFieldMentorTable extends Migration
+class CreateCollegeMentorFieldTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,15 +12,15 @@ class CreateFieldMentorTable extends Migration
      */
     public function up()
     {
-        Schema::create('field_mentor', function (Blueprint $table) {
+        Schema::create('college_mentor_field', function (Blueprint $table) {
             $table->increments('id');
 			$table->integer('field_id')->unsigned()->nullable();
-			$table->integer("mentor_id")->unsigned()->nullable();
+			$table->integer("college_mentor_id")->unsigned()->nullable();
             $table->timestamps();
         });
 		
-		Schema::table('field_mentor', function ($table){
-			$table->foreign("mentor_id")->references("id")->on("college_mentors")->onDelete("cascade");
+		Schema::table('college_mentor_field', function ($table){
+			$table->foreign("college_mentor_id")->references("id")->on("college_mentors")->onDelete("cascade");
 			$table->foreign("field_id")->references("id")->on("fields")->onDelete("cascade");
 		});
     }
@@ -32,6 +32,6 @@ class CreateFieldMentorTable extends Migration
      */
     public function down()
     {
-        Schema::drop("field_mentor");
+        Schema::drop("college_mentor_field");
     }
 }
