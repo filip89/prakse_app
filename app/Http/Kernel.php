859d814244f3477866431657b9dcdcc08befb2b0
filip89 <filip187m@gmail.com>
2016-08-22 14:04:19 +0200

    Update Kernel.php

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -51,7 +51,6 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
 	'admin' => \App\Http\Middleware\Admin::class,
 	'adminOrAuth' => \App\Http\Middleware\AdminOrAuthUser::class,
-        'admin' => \App\Http\Middleware\Admin::class,
-	'adminOrAuth' => \App\Http\Middleware\AdminOrAuthUser::class,
+	'student' => \App\Http\Middleware\Student::class,
     ];
 }
