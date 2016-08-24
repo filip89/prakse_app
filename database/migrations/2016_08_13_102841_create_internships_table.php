a3c92d79555438843b55b72af8562e0545725f0d
filip89 <filip187m@gmail.com>
2016-08-24 17:28:02 +0200

    Update 2016_08_13_102841_create_internships_table.php

--- database/migrations/2016_08_13_102841_create_internships_table.php
+++ database/migrations/2016_08_13_102841_create_internships_table.php
@@ -13,25 +13,25 @@ class CreateInternshipsTable extends Migration
     public function up()
     {
         Schema::create('internships', function (Blueprint $table) {
-            $table->increments('id');
-			$table->integer("student_id")->unsigned();
-			$table->integer("intern_mentor_id")->unsigned()->nullable();
-			$table->integer("college_mentor_id")->unsigned()->nullable();
-			$table->integer("company_id")->unsigned();
-			$table->integer("average_bacc_grade", 3,2);
-			$table->integer("average_master_grade", 3,2)->nullable()->default(0);
-			$table->integer("activity_points");
-			$table->datetime("start_date")->nullable();
-			$table->datetime("end_date")->nullable();
-			$table->integer("duration", 2)->nullable();
-			$table->integer("year", 4)->nullable();
-			$table->string("student_comment", 5000)->nullable();
-			$table->integer("rating_by_student")->nullable();
-			$table->string("intern_mentor_comment", 5000)->nullable();
-			$table->string("college_mentor_comment", 5000)->nullable();
-			$table->integer("confirmation_student")->nullable()->default(0);
-			$table->integer("confirmation_admin")->nullable()->default(0);	
-        		$table->timestamps();
+        	$table->increments('id');
+		$table->integer("student_id")->unsigned();
+		$table->integer("intern_mentor_id")->unsigned()->nullable();
+		$table->integer("college_mentor_id")->unsigned()->nullable();
+		$table->integer("company_id")->unsigned();
+		$table->integer("average_bacc_grade", 3,2);
+		$table->integer("average_master_grade", 3,2)->nullable()->default(0);
+		$table->integer("activity_points");
+		$table->datetime("start_date")->nullable();
+		$table->datetime("end_date")->nullable();
+		$table->integer("duration", 2)->nullable();
+		$table->integer("year", 4)->nullable();
+		$table->string("student_comment", 5000)->nullable();
+		$table->integer("rating_by_student")->nullable();
+		$table->string("intern_mentor_comment", 5000)->nullable();
+		$table->string("college_mentor_comment", 5000)->nullable();
+		$table->integer("confirmation_student")->nullable()->default(0);
+		$table->integer("confirmation_admin")->nullable()->default(0);	
+        	$table->timestamps();
         });
 		
 		Schema::table('internships', function($table) {
