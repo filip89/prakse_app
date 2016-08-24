ac9c76146cd8d0d631ba0271e6be805fd7716273
filip89 <filip187m@gmail.com>
2016-08-24 13:17:44 +0200

    Update Utilities.php

--- app/Utilities.php
+++ app/Utilities.php
@@ -6,11 +6,28 @@ namespace App;
 class Utilities
 {
 	
-    private static $academic_year = array('1. godina preddiplomskog', '2. godina preddiplomskog', '3. godina preddiplomskog', '1. godina diplomskog', '2. godina diplomskog');
+   	private static $academic_year = array('1. godina preddiplomskog', '2. godina preddiplomskog', '3. godina preddiplomskog', '1. godina diplomskog', '2. godina diplomskog');
+	
+	private static $course = array('Financijski menadžment', 'Marketing', 'Menadžment', 'Poduzetništvo', 'Poslovna informatika');
+	
+	private static $desired_month = array(6 => 'Lipanj', 7 => 'srpanj', 8 => 'Kolovoz', 9 => 'Rujan');
+	
 	
 	public static function academicYear($year){
 		
 		return self::$academic_year[$year - 1];
+		
+	}
+	
+	public static function course($course){
+		
+		return self::$course[$course - 1];
+		
+	}
+	
+	public static function desiredMonth($month){
+		
+		return self::$desired_month[$month];
 		
 	}
 }
