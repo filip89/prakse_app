974c703aed34229f1228b56a946015f11d8e15c9
filip89 <filip187m@gmail.com>
2016-08-25 09:51:02 +0200

    Update 2016_08_13_102841_create_internships_table.php

--- database/migrations/2016_08_13_102841_create_internships_table.php
+++ database/migrations/2016_08_13_102841_create_internships_table.php
@@ -18,7 +18,7 @@ class CreateInternshipsTable extends Migration
 		$table->integer("intern_mentor_id")->unsigned()->nullable();
 		$table->integer("college_mentor_id")->unsigned()->nullable();
 		$table->integer("company_id")->unsigned();
-		$table->decimal("average_bacc_grade", 3,2);
+		$table->decimal("average_bacc_grade", 3,2)->nullable()->default(0);
 		$table->decimal("average_master_grade", 3,2)->nullable()->default(0);
 		$table->integer("activity_points");
 		$table->datetime("start_date")->nullable();
