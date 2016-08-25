74b18bc5ae083775e74ecf610d854810e53cb7ce
filip89 <filip187m@gmail.com>
2016-08-25 15:33:41 +0200

    Update Utilities.php

--- app/Utilities.php
+++ app/Utilities.php
@@ -6,35 +6,35 @@ namespace App;
 class Utilities
 {
 	
-   	private static $academic_year = array('1. godina preddiplomskog', '2. godina preddiplomskog', '3. godina preddiplomskog', '1. godina diplomskog', '2. godina diplomskog');
+   	private static $academic_years = array('1. godina preddiplomskog', '2. godina preddiplomskog', '3. godina preddiplomskog', '1. godina diplomskog', '2. godina diplomskog');
 	
-	private static $course = array('Financijski menadžment', 'Marketing', 'Menadžment', 'Poduzetništvo', 'Poslovna informatika');
+	private static $courses = array('Financijski menadžment', 'Marketing', 'Menadžment', 'Poduzetništvo', 'Poslovna informatika');
 	
-	private static $desired_month = array(6 => 'Lipanj', 7 => 'srpanj', 8 => 'Kolovoz', 9 => 'Rujan');
+	private static $desired_months = array(6 => 'Lipanj', 7 => 'srpanj', 8 => 'Kolovoz', 9 => 'Rujan');
 	
-	private static $activity = array('a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10');
+	private static $activities = array('a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10');
 	
 	public static function academicYear($year){
 		
-		return self::$academic_year[$year - 1];
+		return self::$academic_years[$year - 1];
 		
 	}
 	
 	public static function course($course){
 		
-		return self::$course[$course - 1];
+		return self::$courses[$course - 1];
 		
 	}
 	
 	public static function desiredMonth($month){
 		
-		return self::$desired_month[$month];
+		return self::$desired_months[$month];
 		
 	}
 	
 	public static function activity($num){
 		
-		return self::$activity[$num];
+		return self::$activities[$num - 1];
 		
 	}
 
