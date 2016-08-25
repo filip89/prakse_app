c4259e42fbe3c4c8eb672450d559d5fdd729071f
filip89 <filip187m@gmail.com>
2016-08-25 13:40:07 +0200

    Update Utilities.php

--- app/Utilities.php
+++ app/Utilities.php
@@ -12,6 +12,7 @@ class Utilities
 	
 	private static $desired_month = array(6 => 'Lipanj', 7 => 'srpanj', 8 => 'Kolovoz', 9 => 'Rujan');
 	
+	private static $activity = array('a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9', 'a10');
 	
 	public static function academicYear($year){
 		
@@ -28,6 +29,12 @@ class Utilities
 	public static function desiredMonth($month){
 		
 		return self::$desired_month[$month];
+		
+	}
+	
+	public static function activity($num){
+		
+		return self::$activity[$num];
 		
 	}
 
