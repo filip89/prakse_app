e03f807e226a54abe37f97f2ec687ccaf04e5596
filip89 <filip187m@gmail.com>
2016-08-26 19:39:05 +0200

    Update myapplic.blade.php

--- resources/views/myapplic.blade.php
+++ resources/views/myapplic.blade.php
@@ -7,14 +7,14 @@
 			
 			@if(count(Auth::user()->applic) > 0)
 			<p>Izradili ste prijavu.</p>
-			<a href="/apply"><button class="btn btn-info btn-sm">Uredi</button></a>
+			<a href="{{ url('/apply') }}"><button class="btn btn-info btn-sm">Uredi</button></a>
 			<form action="{{ url('applic/'. Auth::user()->id . '/delete') }}" method="POST">
 								{{ csrf_field() }}
 				<button class="btn btn-danger btn-sm">Ukloni</button>
 			</form>
 			@else
 			<p>Niste još izradili prijavu.</p>
-			<a href="/apply"><button class="btn btn-info btn-sm">Prijavi se</button></a>
+			<a href="{{ url('/apply') }}"><button class="btn btn-info btn-sm">Prijavi se</button></a>
 			@endif
         </div>
     </div>
