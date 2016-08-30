2ba3233a192790c799752fe461a3fafdb4006461
RajicDenis <denis.rajic92@gmail.com>
2016-08-30 14:41:05 +0200

    Add files via upload

--- app/Http/Controllers/InternshipController.php
+++ app/Http/Controllers/InternshipController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Session;
+use Carbon\Carbon;
 use App\Http\Requests;
 use App\Internship;
 use App\Company;
@@ -12,7 +13,6 @@ use App\Applic;
 use App\InternMentor;
 use App\CollegeMentor;
 use App\Utilities;
-
 
 class InternshipController extends Controller
 {
@@ -53,12 +53,21 @@ class InternshipController extends Controller
 
         $applic = Applic::all();
         $companies= Company::pluck('name', 'id');
-        $users = User::where('role', 'college_mentor')->pluck('last_name', 'id');
+        $users = User::where('role', 'college_mentor')->orWhere('role', 'intern_mentor')->get();
+       
+        foreach($users as $user) {    
+            if($user->role == 'college_mentor') {
+                $collegeMentor[$user->id] = $user->name.' '.$user->last_name;   
+            } else {
+                $internMentor[$user->id] = $user->name.' '.$user->last_name;
+            }                     
+        } 
 
         return view('internships.create')
             ->with('applic', $applic)
             ->with('companies', $companies)
-            ->with('collegeMentor', $users)
+            ->with('collegeMentor', $collegeMentor)
+            ->with('internMentor', $internMentor)
             ->with('academicYear', $academicYear)
             ->with('course', $course)
             ->with('month', $month);
@@ -73,6 +82,20 @@ class InternshipController extends Controller
     public function store(Request $request)
     {
 
+
+        $this->validate($request, [          
+            'average_bacc_grade' => 'required',
+            'average_master_grade' => 'required',
+            'activity_points' => 'required',
+            'total_points' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required',
+            'duration' => 'required',
+            'year' => 'required|integer|between:2015,2100',
+            'rating_by_student' => 'required|min:1|max:5',         
+        ]);
+
+         
         $internship = new Internship;
 
         $internship->student_id = $request->student_id;
@@ -82,11 +105,12 @@ class InternshipController extends Controller
         $internship->average_master_grade = $request->average_master_grade;
         $internship->activity_points = $request->activity_points;
         $internship->total_points = $request->total_points;
-        $internship->start_date = $request->start_date;
-        $internship->end_date = $request->end_date;
+        $internship->start_date = date('Y-m-d', strtotime($request->start_date));
+        $internship->end_date = date('Y-m-d', strtotime($request->end_date));
         $internship->duration = $request->duration;
         $internship->year = $request->year; 
         $internship->college_mentor_id = $request->college_mentor_id;
+        $internship->intern_mentor_id = $request->intern_mentor_id;
         $internship->student_comment = $request->student_comment;
         $internship->rating_by_student = $request->rating_by_student;
         $internship->intern_mentor_comment = $request->intern_mentor_comment;
@@ -96,6 +120,7 @@ class InternshipController extends Controller
         $internship->save();
 
         return redirect()->route('internships.index');
+        
     }
 
     /**
@@ -107,9 +132,25 @@ class InternshipController extends Controller
 
     public function edit($id)
     {
-        $internship = Internship::with('college_mentor')->find($id);
 
-        return view('internships.edit')->with('internship', $internship);
+        $internship = Internship::with('college_mentor')->find($id);
+        $companies= Company::pluck('name', 'id');
+        $users = User::where('role', 'college_mentor')->orWhere('role', 'intern_mentor')->get();
+       
+        foreach($users as $user) {    
+            if($user->role == 'college_mentor') {
+                $collegeMentor[$user->id] = $user->name.' '.$user->last_name;   
+            } else {
+                $internMentor[$user->id] = $user->name.' '.$user->last_name;
+            }                     
+        } 
+
+        return view('internships.edit')
+            ->with('internship', $internship)
+            ->with('companies', $companies)
+            ->with('collegeMentor', $collegeMentor)
+            ->with('internMentor', $internMentor);
+        
     }
 
     /**
@@ -121,20 +162,32 @@ class InternshipController extends Controller
      */
     public function update(Request $request, $id)
     {
+        $this->validate($request, [       
+            'average_bacc_grade' => 'required',
+            'average_master_grade' => 'required',
+            'activity_points' => 'required',
+            'total_points' => 'required',
+            'start_date' => 'required',
+            'end_date' => 'required',
+            'duration' => 'required',
+            'year' => 'required|integer|between:2015,2100',
+            'rating_by_student' => 'required|min:1|max:5',   
+        ]);
+
         $internship = Internship::find($id);
 
-        //validate
         $internship->company_id = $request->company_id;
         $internship->academic_year = $request->academic_year;
         $internship->average_bacc_grade = $request->average_bacc_grade;
         $internship->average_master_grade = $request->average_master_grade;
         $internship->activity_points = $request->activity_points;
         $internship->total_points = $request->total_points;
-        $internship->start_date = $request->start_date;
-        $internship->end_date = $request->end_date;
+        $internship->start_date = date('Y-m-d', strtotime($request->start_date));
+        $internship->end_date = date('Y-m-d', strtotime($request->end_date));
         $internship->duration = $request->duration;
         $internship->year = $request->year; 
         $internship->college_mentor_id = $request->college_mentor_id;
+        $internship->intern_mentor_id = $request->intern_mentor_id;
         $internship->student_comment = $request->student_comment;
         $internship->rating_by_student = $request->rating_by_student;
         $internship->intern_mentor_comment = $request->intern_mentor_comment;
