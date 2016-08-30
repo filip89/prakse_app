baa728bff84e94a1ecf79e075d21c5e80c51c1ec
filip89 <filip187m@gmail.com>
2016-08-30 22:57:20 +0200

    Add files via upload

--- resources/views/forms/intern_mentor.blade.php
+++ resources/views/forms/intern_mentor.blade.php
@@ -1,112 +1,57 @@
 @extends('layouts.app')
 
+@section('style')
+form {
+	display: inline;
+}
+.action_buttons {
+	display: table;
+	margin: auto;
+}
+table {
+	margin-top: 30px;
+	text-align: center;
+}
+@endsection
+
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Uredi profil</div>
-                <div class="panel-body">
-                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/user/' . $user->id . '/editintern') }}">
-                        {{ csrf_field() }}
-						
-						<div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
-                            <label for="name" class="col-md-4 control-label">Ime</label>
-
-                            <div class="col-md-6">
-                                <input type="text" class="form-control" name="name" value="{{ $user->name }}" required>
-
-                                @if ($errors->has('name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-						
-						<div class="form-group{{ $errors->has('last_name') ? ' has-error' : '' }}">
-                            <label for="last_name" class="col-md-4 control-label">Prezime</label>
-
-                            <div class="col-md-6">
-                                <input type="txt" class="form-control" name="last_name" value="{{ $user->last_name }}" required>
-
-                                @if ($errors->has('last_name'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('last_name') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-						
-						<div class="form-group{{ $errors->has('job_description') ? ' has-error' : '' }}">
-                            <label for="job_description" class="col-md-4 control-label">Radno mjesto</label>
-
-                            <div class="col-md-6">
-                                <input type="text" class="form-control" name="job_description" value="{{ $user->profile->job_description }}">
-
-                                @if ($errors->has('job_description'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('job_description') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-						
-						<div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
-                            <label for="phone" class="col-md-4 control-label">Telefon</label>
-
-                            <div class="col-md-6">
-                                <input type="text" class="form-control" name="phone" value="{{ $user->profile->phone }}">
-
-                                @if ($errors->has('phone'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('phone') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-			@if(Auth::user()->isAdmin())		
-						<div class="form-group{{ $errors->has('company') ? ' has-error' : '' }}">
-                            <label for="company" class="col-md-4 control-label">Tvrtka</label>
-
-                            <div class="col-md-6">
-                                <select class="form-control" name="company" required/>
-										@if(!isset($user->profile->company))
-											<option selected disabled hidden style='display: none' value=''></option>
-											@foreach($companies as $company)
-											<option value="{{$company->id}}">{{$company->name}}</option>
-											@endforeach
-										@else
-											@foreach($companies as $company)
-												@if($user->profile->company->id == $company->id)
-												<option selected value="{{$company->id}}">{{$company->name}}</option>
-												@else
-												<option  value="{{$company->id}}">{{$company->name}}</option>
-												@endif
-											@endforeach
-										@endif
-								</select>
-
-                                @if ($errors->has('company'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('company') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-                        @endif
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <i class="fa fa-btn fa-sign-in"></i> Spremi
-                                </button>
-                            </div>
-                        </div>
-						
-                    </form>
-                </div>
-            </div>
+        <div class="col-md-4 col-md-offset-4">
+			<div class="table-responsive">
+				<table class="table table-striped">
+					<tr><th>Ime:</th><td>{{ $user->name }}</td></tr>
+					<tr><th>Prezime:</th><td>{{ $user->last_name }}</td></tr>
+					<tr><th>Radno mjesto:</th><td>{{ $user->profile->job_description }}</td></tr>
+					<tr><th>Telefon:</th><td>{{ $user->profile->phone }}</td></tr>
+					<tr><th>Tvrtka:</th>
+						<td>
+						@if(!is_null($user->profile->company)) 
+						<a href="{{url('/company/profile/' . $user->profile->company->id)}}">{{ $user->profile->company->name }}</a>
+						@endif 
+						</td>
+					</tr>
+					<tr><th>Mentorira studente:</th>
+						<td>
+						@foreach($internships as $internship) 
+						<a href="{{url('/internship/' . $internship->id)}}">{{ $internship->student->name . " " . $internship->student->last_name }}</a><br/>
+						@endforeach
+						</td>
+					</tr>
+					
+				</table>
+				@if (Auth::user()->id == $user->id || Auth::user()->isAdmin())
+					<div class="action_buttons">
+						<a href="{{ url('user/'. $user->id . '/editintern') }}"><button class="btn btn-info btn-sm" >Uredi</button></a>
+						@if (Auth::user()->isAdmin() && Auth::user()->id != $user->id)	
+							<form action="{{ url('user/'. $user->id . '/delete') }}" method="POST">
+								{{ csrf_field() }}
+								<button class="btn btn-default btn-sm" >Ukloni</button>
+							</form>
+					</div>
+					@endif
+				@endif	
+			</div>
         </div>
     </div>
 </div>
