0a45534c864d482cd7b155434b9697e2fb145a6d
filip89 <filip187m@gmail.com>
2016-08-30 16:15:46 +0200

    Update app.php

--- config/app.php
+++ config/app.php
@@ -204,7 +204,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
 	'Form' => Collective\Html\FormFacade::class,
 	'Html' => Collective\Html\HtmlFacade::class,
-	'Utilities' => App\Utilities::class;
+	'Utilities' => App\Utilities::class,
 
     ],
 
