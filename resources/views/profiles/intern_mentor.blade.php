113d9a26c531c74daecedb160c97d3f0b5341e8a
filip89 <filip187m@gmail.com>
2016-08-30 23:00:02 +0200

    Add files via upload

--- resources/views/profiles/intern_mentor.blade.php
+++ resources/views/profiles/intern_mentor.blade.php
@@ -17,14 +17,14 @@ table {
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-10 col-md-offset-1">
+        <div class="col-md-4 col-md-offset-4">
 			<div class="table-responsive">
 				<table class="table table-striped">
-					<tr><th>Ime</th><td>{{ $user->name }}</td></tr>
-					<tr><th>Prezime</th><td>{{ $user->last_name }}</td></tr>
-					<tr><th>Radno mjesto</th><td>{{ $user->profile->job_description }}</td></tr>
-					<tr><th>Telefon</th><td>{{ $user->profile->phone }}</td></tr>
-					<tr><th>Tvrtka</th>
+					<tr><th>Ime:</th><td>{{ $user->name }}</td></tr>
+					<tr><th>Prezime:</th><td>{{ $user->last_name }}</td></tr>
+					<tr><th>Radno mjesto:</th><td>{{ $user->profile->job_description }}</td></tr>
+					<tr><th>Telefon:</th><td>{{ $user->profile->phone }}</td></tr>
+					<tr><th>Tvrtka:</th>
 						<td>
 						@if(!is_null($user->profile->company)) 
 						<a href="{{url('/company/profile/' . $user->profile->company->id)}}">{{ $user->profile->company->name }}</a>
@@ -38,20 +38,19 @@ table {
 						@endforeach
 						</td>
 					</tr>
-					@if (Auth::user()->id == $user->id || Auth::user()->isAdmin())
-					<tr>
-						<td>
-							<a href="{{ url('user/'. $user->id . '/editintern') }}"><button class="btn btn-info btn-sm" >Uredi</button></a>
+					
+				</table>
+				@if (Auth::user()->id == $user->id || Auth::user()->isAdmin())
+					<div class="action_buttons">
+						<a href="{{ url('user/'. $user->id . '/editintern') }}"><button class="btn btn-info btn-sm" >Uredi</button></a>
 						@if (Auth::user()->isAdmin() && Auth::user()->id != $user->id)	
 							<form action="{{ url('user/'. $user->id . '/delete') }}" method="POST">
 								{{ csrf_field() }}
 								<button class="btn btn-default btn-sm" >Ukloni</button>
 							</form>
-						</td>			
-					</tr>
+					</div>
 					@endif
-					@endif	
-				</table>
+				@endif	
 			</div>
         </div>
     </div>
