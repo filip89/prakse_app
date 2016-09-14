5f011df15aef30f6bf0b6c5ffa315960c4ae85bf
filip89 <filip187m@gmail.com>
2016-09-14 11:10:19 +0200

    Update 2016_08_13_102841_create_internships_table.php

--- database/migrations/2016_08_13_102841_create_internships_table.php
+++ database/migrations/2016_08_13_102841_create_internships_table.php
@@ -31,8 +31,8 @@ class CreateInternshipsTable extends Migration
 		$table->integer("rating_by_student")->nullable();
 		$table->string("intern_mentor_comment", 5000)->nullable();
 		$table->string("college_mentor_comment", 5000)->nullable();
-		$table->integer("confirmation_student")->nullable()->default(0);
-		$table->integer("confirmation_admin")->nullable()->default(0);	
+		$table->integer("confirmation_student")->nullable();
+		$table->integer("confirmation_admin")->nullable();	
 		$table->integer("status")->default(1);	
         	$table->timestamps();
         });
