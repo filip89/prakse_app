8255726b6cd8c37cb83032c357fe93c224ec5b40
filip89 <filip187m@gmail.com>
2016-09-16 16:45:44 +0200

    Add files via upload

--- app/Utilities.php
+++ app/Utilities.php
@@ -9,11 +9,17 @@ class Utilities
 	
 	private static $courses = array('Financijski menadžment', 'Marketing', 'Menadžment', 'Poduzetništvo', 'Poslovna informatika');
 	
-	private static $desired_months = array(6 => 'Lipanj', 7 => 'Srpanj', 8 => 'Kolovoz', 9 => 'Rujan');
+	private static $desired_months = array(1 => 'Siječanj', 2 => 'Veljača', 3 => 'Ožujak', 4 => 'Travanj', 5 => 'Svibanj', 6 => 'Lipanj', 7 => 'Srpanj', 8 => 'Kolovoz', 9 => 'Rujan', 10 => 'Listopad', 11 => 'Studeni', 12 => 'Prosinac');
 	
 	private static $activities = array('Članstvo u studentskoj i/ili civilnim udrugama; rad u studentskoj organizaciji koja je registrirana ili upisana u upisnik studentskih organizacija.', 'Studentsko predstavljanje u sveučilišnim ili drugim tijelima relevantnim za sustav znanosti i visokog obrazovanja (Senat, Studentsko zbor, Smotra).', 'Rad na znanstvenom projektu; izlaganje na znanstvenom i/ili stručnom skupu; uređivanje studentskih, znanstvenih ili stručnih časopisa (glavni urednik, tajnik/tajnica, članovi uredništva).', 'Izlaganje ili sudjelovanje u organizaciji ljetnih škola, znanstvenih i/ili stručnih skupova.', 'Seminari/radionice (organizacija/sudjelovanje).', 'Sudjelovanje u Erasmus programu i/ili rd u inozemstvu tijekom studija.', 'Rad preko studentskog centra.', 'Demonstratura tijekom preddiplomskog i/ili diplomskog studija.', 'Rektorova i/ili dekanova nagrada.', 'Sudjelovanje u studentskim natjecanjima i ostvareno jedno od prva tri mjesta.');
 	
+	private static $counties = array(0 => 'Zagrebačka županija', 1 => 'Krapinsko-zagorska županija', 2 => 'Sisačko-moslavačka županija', 3 => 'Međimurska županija', 4 => 'Karlovačka županija', 5 => 'Varaždinska županija', 6 => 'Koprivničko-križevačka županija', 7 => 'Bjelovarsko-bilogorska županija', 8 => 'Primorsko-goranska županija', 9 => 'Ličko-senjska županija', 10 => 'Virovitičko-podravska županija', 11 => 'Požeško-slavonska županija', 12 => 'Brodsko-posavska županija', 13 => 'Zadarska županija', 14 => 'Osječko-baranjska županija', 15 => 'Šibensko-kninska županija', 16 => 'Splitsko-dalmatinska županija', 17 => 'Vukovarsko-srijemska županija', 18 => 'Istarska županija', 19 => 'Dubrovačko-neretvanska županija', 20 => 'Grad Zagreb');
+	
 	public static function academicYear($year){
+		
+		if(!isset($year)){
+			return false;
+		}
 		
 		return self::$academic_years[$year - 1];
 		
@@ -22,7 +28,7 @@ class Utilities
 	public static function course($course){
 		
 		if(!isset($course)){
-			return;
+			return false;
 		}
 		
 		return self::$courses[$course - 1];
@@ -30,7 +36,7 @@ class Utilities
 	}
 	
 	public static function desiredMonth($month){
-		
+
 		if(!isset($month)){
 			return false;
 		}
@@ -40,11 +46,11 @@ class Utilities
 	}
 	
 	public static function activity($num){
-		
+				
 		if(!isset($num)){
-			return;
+			return false;
 		}
-		
+			
 		return self::$activities[$num - 1];
 		
 	}
@@ -58,6 +64,18 @@ class Utilities
 		}
 		
 		return 0;
+		
+	}
+	
+	public static function county($county = "all"){
+				
+		if($county == "all"){
+			
+			return self::$counties;
+			
+		}		
+				
+		return self::$counties[$county];
 		
 	}
 
