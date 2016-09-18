463c11fbb9e7b340f9baf6e1e401a1d67e332288
RajicDenis <denis.rajic92@gmail.com>
2016-09-18 10:59:33 +0200

    Add files via upload

--- config/app.php
+++ config/app.php
@@ -157,6 +157,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        Barryvdh\DomPDF\serviceProvider::class,
+
     ],
 
     /*
@@ -202,9 +204,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-	'Form' => Collective\Html\FormFacade::class,
-	'Html' => Collective\Html\HtmlFacade::class,
-	'Utilities' => App\Utilities::class,
+    	'Form' => Collective\Html\FormFacade::class,
+    	'Html' => Collective\Html\HtmlFacade::class,
+    	'Utilities' => App\Utilities::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
 
     ],
 
