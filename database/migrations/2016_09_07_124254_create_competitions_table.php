6fb25e7bb839b7eb75acae6efc9460d84c407a21
filip89 <filip187m@gmail.com>
2016-09-19 13:26:43 +0200

    Add files via upload

---
+++ database/migrations/2016_09_07_124254_create_competitions_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompetitionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+	 *
+	 * status: 0 - not active; 1 - active
+	 *
+     */
+    public function up()
+    {
+        Schema::create('competitions', function (Blueprint $table) {
+        	$table->increments('id');
+			$table->integer("status");
+			$table->integer("year");
+			$table->string("name", 100);
+			$table->integer("internships_available")->default(100);
+       		$table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('competitions');
+    }
+}
