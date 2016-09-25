b9b611da1ce2c0ea9ef5244617b1ee7c563bc45f
filip89 <filip187m@gmail.com>
2016-09-25 19:52:21 +0200

    Add files via upload

---
+++ resources/views/forms/complaint.blade.php
@@ -0,0 +1,51 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+			@if(Session::has('status'))
+				<div class="alert {{ Session::get('alert_type') }} fade in">
+				<a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+				{{ Session::get('status') }}
+			</div>
+			@endif
+			@if(Auth::user()->hasUnresolvedComplaint())
+				<h3 style="margin-bottom:20px;" class="notice">*Trenutno imate neriješene pritužbe u našoj bazi.</h3>
+			@endif
+            <div class="panel panel-warning">
+                <div class="panel-heading"><i class="fa fa-btn fa-pencil-square-o" aria-hidden="true"></i>{{ Auth::user()->name . ' ' . Auth::user()->last_name }} pritužba</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ url('/complaint/create') }}">
+                        {{ csrf_field() }}
+						
+						<div class="form-group{{ $errors->has('content') ? ' has-error' : '' }}">
+                            <label for="content" class="col-md-2 control-label">Pritužba: </label>
+
+                            <div class="col-md-10">
+                                <textarea type="text" class="form-control" style="resize:vertical;height:200px;max-height:500px" name="content" placeholder="Ovdje upišite pritužbu od najviše 10 000 znakova" required></textarea>
+
+                                @if ($errors->has('content'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('content') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+						
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-2">
+                                <button type="submit" class="btn btn-primary">
+                                    <i class="fa fa-btn fa-sign-in"></i> Pošalji
+                                </button>
+                            </div>
+                        </div>
+						
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
