916d9fcc922efecabb666d815621a255c5330dfb
filip89 <filip187m@gmail.com>
2016-09-25 19:58:34 +0200

    Add files via upload

---
+++ database/migrations/2016_09_25_131106_create_complaints_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateComplaintsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+		 Schema::create('complaints', function (Blueprint $table) {
+        	$table->increments('id');
+			$table->integer("student_id")->unsigned();
+			$table->string("content", 10000);
+			$table->integer("status")->default(0);
+        	$table->timestamps();
+        });
+		
+		Schema::table('complaints', function($table) {
+			$table->foreign("student_id")->references("id")->on("users")->onDelete("cascade");
+		});
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('complaints');
+    }
+}
