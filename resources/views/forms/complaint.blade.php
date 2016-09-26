49deb2a32752a4cd00fcd172406e93850d6134c2
filip89 <filip187m@gmail.com>
2016-09-26 13:47:02 +0200

    Add files via upload

--- resources/views/forms/complaint.blade.php
+++ resources/views/forms/complaint.blade.php
@@ -19,6 +19,20 @@
                     <form class="form-horizontal" role="form" method="POST" action="{{ url('/complaint/create') }}">
                         {{ csrf_field() }}
 						
+						<div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-2 control-label">E-mail: </label>
+
+                            <div class="col-md-10">
+                                <input type="email" class="form-control" name="email" placeholder="e-mail na koji želite dobiti odgovor" required/>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+						
 						<div class="form-group{{ $errors->has('content') ? ' has-error' : '' }}">
                             <label for="content" class="col-md-2 control-label">Pritužba: </label>
 
