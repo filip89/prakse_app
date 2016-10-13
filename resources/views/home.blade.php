c8e777969bc87fd1b27ccc731509aa081fe634c1
filip89 <filip187m@gmail.com>
2016-10-13 17:16:55 +0200

    Add files via upload

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,27 +9,23 @@
 						<div class="panel-body">Trenutno nema otvorenog natječaja.</div>
 					</div>
 				@endif
-				@if(Utilities::competitionStatus() != 0)
-				
-						@if(Utilities::competitionStatus() == 1)
-						<div class="panel panel-default">
-							<div class="panel-heading">{{ $competition->created_at->format('d. m. Y.') }}</div>
-							<div class="panel-body">
-								<p>Objavljen je natječaj za prakse koji traje do ponoći {{ date_create($competition->end_date)->format('d. m. Y.') }}.<br/></p>
-								<p>{{ $competition->message }}</p>
-								<p>Postupak prijava za praksu možete pogledati <a href="#">ovdje.</a></p>
-							</div>
+				@if(Utilities::competitionStatus() == 1)
+					<div class="panel panel-default">
+						<div class="panel-heading">{{ $competition->created_at->format('d. m. Y.') }}</div>
+						<div class="panel-body">
+							<p>Objavljen je natječaj za prakse koji traje do ponoći {{ date_create($competition->end_date)->format('d. m. Y.') }}.<br/></p>
+							<p>{{ $competition->message }}</p>
+							<p>Postupak prijava za praksu možete pogledati <a href="#">ovdje.</a></p>
 						</div>
-						@elseif(Utilities::competitionStatus() == 2)
-						<div class="panel panel-default">
-							<div class="panel-heading">{{ date_create($competition->end_date)->format('d. m. Y.') }}</div>
-							<div class="panel-body">
-								<p>Natječaj objavljen {{ $competition->created_at->format('d. m. Y.') }} je završio, u tijeku je postupak dodjela praksi.</p>
-								<p>{{ $competition->message }}</p>
-							</div>
+					</div>
+				@elseif(Utilities::competitionStatus() == 2)
+					<div class="panel panel-default">
+						<div class="panel-heading">{{ date_create($competition->end_date)->format('d. m. Y.') }}</div>
+						<div class="panel-body">
+							<p>Natječaj objavljen {{ $competition->created_at->format('d. m. Y.') }} je završio, u tijeku je postupak dodjela praksi.</p>
 						</div>
-						@endif	
-				@endif
+					</div>
+				@endif	
 				@if(Utilities::competitionExists())
 				<div class="panel panel-default">
 					<div class="panel-heading">{{ date_create($lastCompetition->results_date)->format('d. m. Y.') }}</div>
